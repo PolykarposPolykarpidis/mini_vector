9ef25ab4e7a032be5b704c24c247df1b1e5d97c3
polykarpospolykarpidis <polykarpospolykarpidis@gmail.com>
2025-08-31 19:10:29 +0300

    first commit - structure

---
+++ include/Vector.h
@@ -0,0 +1,15 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <iostream>
+
+template<typename T>
+class Vector{
+public:
+
+private:
+
+};
+
+
+#endif // VECTOR_H
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Vector.h"
+
+int main() {
+    Vector<int> v;
+    // v.push_back(10);
+    // v.push_back(20);
+    // v.push_back(30);
+
+    // for (size_t i = 0; i < v.size(); i++)
+    //     std::cout << v[i] << " ";
+    // std::cout << std::endl;
+
+    return 0;
+}
